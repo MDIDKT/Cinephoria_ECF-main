9fc62433b220e5a9d431ac5e88da98f638b203a9
MDIDKT <152218331+MDIDKT@users.noreply.github.com>
2024-12-22 08:46:43 +0100

    ajout de gesdinet/jwt-refresh-token-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
